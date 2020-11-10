15d4ae8b4ea87a1d64d14cefe538e7854bf56856
majidalaeinia <alaeinia.maijd@gmail.com>
2020-11-10 17:55:09 +0330

    [WIP]

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use MajidAlaeinnia\Khoolios\Http\Controllers\PingPongController;
+
 
 Route::get('ping-get', function () {
     return 'pong-get';
 });
 
-Route::get('ping-controller-get', [PingPongController::class, 'index'])->name('ping-pong.index');
+Route::get('ping-controller-get', [\MajidAlaeinnia\Khoolios\Http\Controllers\PingPongController::class, 'index'])->name('ping-pong.index');
